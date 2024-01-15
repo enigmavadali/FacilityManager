d306d9e7aa13de2cb01fd12baa2d6ca0bf9c1759
Rohit Vadali <vadalirohit@gmail.com>
2024-01-14 17:38:22 -0800

    Add Filewatch service

---
+++ src/main/java/org/hca/facility/FacilityManager/service/FileMonitoringService.java
@@ -0,0 +1,52 @@
+package org.hca.facility.FacilityManager.service;
+
+import jakarta.annotation.PostConstruct;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Service
+public class FileMonitoringService {
+
+    private static final Logger LOGGER = LogManager.getLogger(FileMonitoringService.class);
+
+    @Value("${dir.monitor.path}")
+    private String dirPath;
+
+    @PostConstruct
+    public void init(){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            try{
+                LOGGER.info("Starting file watch");
+                monitorFileChanges(dirPath);
+            }catch(Exception e){
+                LOGGER.error("Error occurred while file monitoring",e);
+            }
+        });
+        executorService.shutdown();
+    }
+
+    private void monitorFileChanges(String dirPath) throws Exception{
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+
+        Path path = Paths.get(dirPath);
+        path.register(watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY);
+
+        WatchKey key;
+        while((key = watchService.take()) != null){
+            for(WatchEvent<?> event : key.pollEvents()){
+                LOGGER.info("event kind " + event.kind() + " detected. FIle affected " + event.context());
+            }
+            key.reset();
+        }
+    }
+}
